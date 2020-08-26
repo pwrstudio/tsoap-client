502d5ce591606fdddcc035af03dd63f67e23c551
pwrstudio <cell@pwr.site>
2020-08-26 14:16:39 +0200

    Switch back to main server...

--- src/World.svelte
+++ src/World.svelte
@@ -94,9 +94,9 @@
   let pathGraphics = {};
 
   // COLYSEUS
-  const client = new Colyseus.Client("ws://localhost:2567");
+  // const client = new Colyseus.Client("ws://localhost:2567");
   // const client = new Colyseus.Client("ws://18.194.21.39:2567");
-  // const client = new Colyseus.Client("wss://gameserver.tsoap.dev");
+  const client = new Colyseus.Client("wss://gameserver.tsoap.dev");
 
   // PIXI
   let app = {};
