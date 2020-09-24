dd1b9ac2b86347f7309eed101cf86213fae93ba6
Hanna Nilsson <hannanilsson@Hannas-MBP.fritz.box>
2020-09-24 18:15:15 +0200

    Style audiochat box

--- src/AudioChat.svelte
+++ src/AudioChat.svelte
@@ -18,7 +18,6 @@
 
   const dispatch = createEventDispatcher()
 
-  // const server = 'http://54.93.169.243:8088/janus';
   const server = "https://janus.tsoap.dev"
   let username = ""
 
@@ -260,7 +259,7 @@
   .audioChatContainer {
     position: fixed;
     width: auto;
-    min-width: 500px;
+    min-width: 400px;
     background: $COLOR_LIGHT;
     height: auto;
     line-height: 2em;
@@ -270,7 +269,6 @@
     padding: 10px;
     border-radius: 4px;
     font-size: $FONT_SIZE_BASE;
-    display: flex;
     font-family: $mono-stack;
 
     padding-left: 15px;
@@ -303,18 +301,46 @@
       flex-direction: row;
       width: 100%;
       height: 30px;
+
+      .microphone {
+        opacity: 0.5;
+      }
     }
 
     .userlist {
       min-height: 200px;
-    }
-    .user {
-      padding: 20px;
-      margin-bottom: 10px;
-    }
+      margin-top: 20px;
 
-    .talking {
-      background: lightgreen;
+      .user {
+        margin-bottom: 10px;
+        width: 100%;
+        line-height: 3em;
+        height: 3em;
+
+        .icon {
+          float: left;
+          height: 3em;
+          width: 3em;
+          border-radius: 3em;
+          margin-right: 2em;
+          background: $COLOR_DARK;
+        }
+
+        .name {
+          float: left;
+        }
+        .speaking {
+          float: right;
+          display: flex;
+          align-items: center;
+          height: 3em;
+          padding-right: 15px;
+
+          svg {
+            opacity: 0.5;
+          }
+        }
+      }
     }
   }
 </style>
@@ -328,7 +354,15 @@
     <div class="text">
       {userList.length} users in room <strong>{roomName}</strong>
     </div>
-    <div class="microphone">(mute)</div>
+    <div class="microphone">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"><path d="M0 0h24v24H0z" fill="none" />
+        <path
+          d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" /></svg>
+    </div>
     <div
       class="button leave"
       on:click={(e) => {
@@ -342,8 +376,17 @@
   <div class="userlist">
     {#each userList as user (user.id)}
       <div class="user {user.id}" class:talking={user.talking} transition:fade>
-        {user.display}
-        {#if user.isSelf}(*){/if}
+        <div class="icon" />
+        <div class="name">{user.display}</div>
+        <div class="speaking">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 0 24 24"
+            width="24"><path d="M0 0h24v24H0z" fill="none" />
+            <path
+              d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" /></svg>
+        </div>
       </div>
     {/each}
   </div>
