c29635a34209b6e860afbe48c861c35817da9334
pwrstudio <pwrpntr@gmail.com>
2020-10-20 16:04:10 +0200

    Add sentry. Fix tracking.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import App from "./App.svelte"
 
-// Sentry.init({ dsn: 'https://9b240ab9fa0746c2a1748b32489b5837@o433904.ingest.sentry.io/5390057' });
+Sentry.init({
+  dsn: 'https://654df1d18dc64c9689fda729c827c4e8@o433904.ingest.sentry.io/5473110'
+});
 
 const app = new App({
   target: document.body,
